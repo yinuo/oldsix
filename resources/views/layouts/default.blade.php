624cde78d596481ccbcc5da095999b3082f65705
vagrant <vagrant@homestead>
2018-04-11 11:25:36 +0000

    Finish login

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'oldsix App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
-  </head>
-  <body>
+    <head>
+        <title>@yield('title', 'oldsix App') - Laravel 入门教程</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
         @include('layouts._header') 
 
-      <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            @include('shared._messages')
-            @yield('content')
-            @include('layouts._footer')
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                @include('shared._messages')
+                @yield('content')
+                @include('layouts._footer')
+                </div>
         </div>
-      </div>
 
-  </body>
+        <script src="/js/app.js"></script>
+    </body>
 </html>

bd6bf1454617309de92451d2f4eab44412aa0596
vagrant <vagrant@homestead>
2018-04-16 08:27:04 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -103,8 +103,6 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'aufree@yousails.com';
-        $name =  'Aufree';
         $to = $user->email;
         $subject="感谢注册 Sample 应用！请确认你的邮箱。";
 
